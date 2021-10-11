46548bf393faa08f239b2608c2d8fefea63b5d4e
minhquang19 <doanminhquangdev@gmail.com>
2021-10-11 23:00:19 +0700

    Debug Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,11 +16,8 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://moonlighthotel.herokuapp.com')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Credentials', 'true')
-            ->header('Access-Control-Allow-Headers', 'X-CSRF-Token');
+        return $next($request);
+
 
     }
 }
