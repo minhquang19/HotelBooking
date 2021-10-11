6b76775d377f20f326a9bf7492c059f2a8f141ef
minhquang19 <doanminhquangdev@gmail.com>
2021-10-11 23:15:05 +0700

    Debug Cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,8 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Origin', 'http://moonlighthotel.herokuapp.com/');
+header('Access-Control-Allow-Methods', '*');
+header('Access-Control-Allow-Credentials', 'true');
+header('Access-Control-Allow-Headers', '*');
 /*
 |--------------------------------------------------------------------------
 | Create The Application
