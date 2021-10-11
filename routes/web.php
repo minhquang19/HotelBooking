fe508380fa30f3fa9fe3ad3d45b69f6f21d93761
minhquang19 <doanminhquangdev@gmail.com>
2021-10-11 15:27:17 +0700

    Debug CORS

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ use App\Http\Controllers\frontEnd\contactController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
+header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
 Auth::routes();
 
 Route::prefix('')->group(function(){
