fafffbe400a171285b068a3c33237b47d14716fe
minhquang19 <doanminhquangdev@gmail.com>
2021-10-16 01:42:52 +0700

    Format Code

--- app/Repositories/Admins/Admin/AdminRepo.php
+++ app/Repositories/Admins/Admin/AdminRepo.php
@@ -47,7 +47,7 @@ class AdminRepo extends BaseAdminRepo implements AdminRepoInterface
     public function getStatisticalByDay($listDay)
     {
         $revenues = Booking::whereMonth('created_at',date('m'))
-                                 ->select(\DB::raw('sum(totalPrice_Vi) as totalMoney'),\DB::raw('DATE(created_at) as day'))
+                                 ->select(\DB::raw('sum(totalPrice) as totalMoney'),\DB::raw('DATE(created_at) as day'))
                                  ->groupBy('day')
                                  ->get()->toArray();
         $arrRevenue = [];
